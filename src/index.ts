4dec9a55d2ac69e54eafa226a2c547bed679e5b1
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-06-07 21:29:23 +0200

    remove

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 import scraper from "./scraper";
-scraper.downloadHTML();
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const htmlFile = path.join(path.dirname(fileURLToPath(import.meta.url)), "./../out/gipfel.html");
+if (!fs.existsSync(htmlFile)) {
+    console.log("download...")
+  scraper.downloadHTML(htmlFile);
+}
+scraper.parseToJSON(htmlFile,"");
--- src/scraper.ts
+++ src/scraper.ts
@@ -1,43 +1,54 @@
 import * as cheerio from "cheerio";
-import got from "got"
-import { createWriteStream } from "fs";
+import got from "got";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const downloadHTML = async () => new Promise<void>((resolve,reject) =>{
-  const downloadStream = got.stream.post(
-    "http://teufelsturm.de/wege/suche.php",
-    {
-      json: {
-        start: "0",
-        gebiet: "0",
-        text: "",
-        skala_von: "",
-        skala_bis: "",
-        datum: "1",
-        sortiert: "1",
-        benutzer: "",
-        bewertungen: "",
-        avgbewertung: "",
-        gipfelnr: "",
-        volltext: "",
-        anzahl: "Alle",
-        abschicken: "anzeigen",
-      },
-    }
-  );
-  const fileWriterStream = createWriteStream("out.html");
+const downloadHTML = async (htmlPath: string) =>
+  new Promise<void>((resolve, reject) => {
+    const downloadStream = got.stream.post(
+      "http://teufelsturm.de/wege/suche.php",
+      {
+        json: {
+          start: "0",
+          gebiet: "0",
+          text: "",
+          skala_von: "",
+          skala_bis: "",
+          datum: "1",
+          sortiert: "1",
+          benutzer: "",
+          bewertungen: "",
+          avgbewertung: "",
+          gipfelnr: "",
+          volltext: "",
+          anzahl: "Alle",
+          abschicken: "anzeigen",
+        },
+      }
+    );
+    const fileWriterStream = fs.createWriteStream(htmlPath);
 
-  downloadStream.pipe(fileWriterStream);
-  downloadStream.on("error", (error) => {
+    downloadStream.pipe(fileWriterStream);
+    downloadStream.on("error", (error) => {
+      reject(error);
+    });
+
+    fileWriterStream
+      .on("error", (error) => {
         reject(error);
+      })
+      .on("finish", () => {
+        resolve();
+      });
   });
 
-  fileWriterStream
-    .on("error", (error) => {
-        reject(error);
-    })
-    .on("finish", () => {
-        resolve();
+const parseToJSON = async (htmlPath: string, jsonPath: string) =>
+  new Promise<void>((resolve, reject) => {
+    const $ = cheerio.load(fs.readFileSync(htmlPath));
+    $("body:nth-child(2) ").each((i, element) => {
+      console.log(element);
     });
-});
+  });
 
-export default { downloadHTML };
+export default { downloadHTML, parseToJSON };
